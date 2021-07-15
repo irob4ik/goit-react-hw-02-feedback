6aae9b2922856c24d54a09a74491673ec98a9ad7
Roberts Kurmis <theroberto@inbox.lv>
2021-07-15 23:52:41 +0300

    stated Section and Statistics components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 
+import Statistics from './components/statistics/Statistics';
+import Section from './components/section/Section';
+
 import styles from './container.module.css';
 
 class App extends Component {
   state = {
-    }
+    good: 100,
+    neutral: 10,
+    bad: 0
+  };
+  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const positivePercentage = ((good / (good + neutral + bad))*100).toFixed(0) + "%";
+    return positivePercentage;
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const percentage = this.countPositiveFeedbackPercentage();
+
     return (
       <div className={styles.container}>
+        <Section title="Please leave feedback">
+          
+        </Section>
 
+        <Section title="Statistics">
+          <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={percentage}
+          />   
+        </Section>
       </div>
     );
   }
