ebca2ccc020594b6d7dc2e01f4fe26734509f7f8
Roberts Kurmis <theroberto@inbox.lv>
2021-07-15 22:44:35 +0300

    setting up React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import styles from './container.module.css';
+
+class App extends Component {
+  state = {
+    }
+  render() {
+    return (
+      <div className={styles.container}>
+
+      </div>
+    );
+  }
+}
+export default App;
